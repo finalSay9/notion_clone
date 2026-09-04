ca6a0566d69e1fb3e6c4331d6b59ca1135d4a1f1
finalSay9 <chimwazaevan1@gmail.com>
2026-09-04 15:35:14 +0200

    dto creating here

--- services/users-service/src/auth/dto/create-user.dto.ts
+++ services/users-service/src/auth/dto/create-user.dto.ts
@@ -1,12 +1,7 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, Matches, MinLength } from "class-validator";
 
 
 export class CreateUserDto {
-
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(3)
-    name!: string
 
     @IsEmail()
     @IsNotEmpty()
@@ -14,7 +9,11 @@ export class CreateUserDto {
 
     @IsString()
     @IsNotEmpty()
-    @MinLength(6)
+    @MinLength(8)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
+    message:
+      'Password must contain uppercase, lowercase, number and special character',
+    })
     password!: string
     
 }
