82802630d137a4a1ee74ffe9d60c4a91139ff185
finalSay9 <chimwazaevan1@gmail.com>
2026-09-04 15:31:25 +0200

    dto creating here

--- services/users-service/src/auth/dto/create-user.dto.ts
+++ services/users-service/src/auth/dto/create-user.dto.ts
@@ -6,5 +6,15 @@ export class CreateUserDto {
     @IsString()
     @IsNotEmpty()
     @MinLength(3)
+    name!: string
+
+    @IsEmail()
+    @IsNotEmpty()
+    email!: string
+
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(6)
+    password!: string
     
 }
