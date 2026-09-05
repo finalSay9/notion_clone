1a5dc1f5780254096440550753e2646d4e89f75a
finalSay9 <chimwazaevan1@gmail.com>
2026-09-05 14:33:03 +0200

    fixed the prisma problem issue

--- services/users-service/src/auth/auth.service.ts
+++ services/users-service/src/auth/auth.service.ts
@@ -1,5 +1,22 @@
-import { Injectable } from '@nestjs/common';
-
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private prisma: PrismaService){}
+
+    async createUser(dto: CreateUserDto){
+        /**
+         * checking if the email
+         * already exist in the system
+         */
+        const existingEmail  = await this.prisma.user.findUnique({
+            where:  {email: dto.email}
+        })
+        //if the email exist
+        if(existingEmail){
+            throw new ConflictException("this email already exist")
+        }
+    }
+}
