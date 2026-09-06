86651cdcf7febda4c0f1b721d8a4fdf1dfa91e01
finalSay9 <chimwazaevan1@gmail.com>
2026-09-06 13:59:14 +0200

    issueing a jwt

--- services/users-service/src/auth/auth.module.ts
+++ services/users-service/src/auth/auth.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+
+  imports: [],
   
   controllers: [AuthController],
   providers: [AuthService, PrismaService]
