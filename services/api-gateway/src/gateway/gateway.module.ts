f032ac0486ae64681d1845a3755cd1642bfc829e
finalSay9 <chimwazaevan1@gmail.com>
2026-09-11 20:51:51 +0200

    config

--- services/api-gateway/src/gateway/gateway.module.ts
+++ services/api-gateway/src/gateway/gateway.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
+  imports: [
+
+  ],
   controllers: [GatewayController],
   providers: [GatewayService]
 })
