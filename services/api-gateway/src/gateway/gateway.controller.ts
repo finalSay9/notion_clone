1d3ab6b314d2205c6ade37573eeb36a1e4f8d1bc
finalSay9 <chimwazaevan1@gmail.com>
2026-09-12 07:12:17 +0200

    creating a user in gateway controller

--- services/api-gateway/src/gateway/gateway.controller.ts
+++ services/api-gateway/src/gateway/gateway.controller.ts
@@ -12,6 +12,22 @@ import {
 import { ClientProxy } from "@nestjs/microservices";
 import { firstValueFrom } from "rxjs";
 import { CreateUserDto } from "./dto/register.dto";
+import { GatewayService } from "./gateway.service";
 
 @Controller('gateway')
-export class GatewayController {}
+export class GatewayController {
+
+  constructor(
+     @Inject('USERS_SERVICE') private readonly usersService: ClientProxy,
+     private readonly gatewaySerive: GatewayService
+  ){}
+
+  /**
+   * Registering a 
+   * user
+   */
+  @Post('register')
+  async createUser() {
+    
+  }
+}
