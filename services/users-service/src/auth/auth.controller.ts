7e885d1863462888024819d48792da335fd74ec4
finalSay9 <chimwazaevan1@gmail.com>
2026-09-12 07:00:46 +0200

    creating a gateway module and wire it up with TCP clients

--- services/users-service/src/auth/auth.controller.ts
+++ services/users-service/src/auth/auth.controller.ts
@@ -1,12 +1,10 @@
-import { Controller,Body, Post, UseGuards, Req, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './guards/local.guard';
-import { JwtAuthGuard } from './guards/jwt.guard';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 
-@Controller('auth')
+@Controller()
 export class AuthController {
 
     constructor(private authService: AuthService){}
@@ -17,11 +15,7 @@ export class AuthController {
     return await this.authService.createUser(createUserDto);
    }
 
-  /**
-   * here the useguards
-   * block the direct
-   * access to the endpoint
-   */ 
+  
     @MessagePattern({cmd: 'auth.login'})
     login(@Payload() loginDto: any) {
       return this.authService.login(loginDto);
