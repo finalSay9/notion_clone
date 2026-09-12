441bb25b4f69aa030a66d2cf31caae38ab8d97f5
finalSay9 <chimwazaevan1@gmail.com>
2026-09-12 15:49:45 +0200

    started the document project

--- services/documents-service/src/documents/documents.service.ts
+++ services/documents-service/src/documents/documents.service.ts
@@ -1,14 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { DocumentDto } from './dto/create-document.dto';
+import { ClientProxy } from '@nestjs/microservices';
+
 
 @Injectable()
 export class DocumentsService {
-    constructor(private prisma: PrismaService){}
+    constructor(
+        @Inject("USERS_SERVICE") private readonly userClient: ClientProxy,
+        private prisma: PrismaService,
+        
+    ){}
 
 
-    async createDocument(document: DocumentDto, userId: string){
-
-
+    async createDocument(dto: DocumentDto, userId: string){
+        return this.prisma.document.create({
+            data: {
+                title: dto.title,
+                content: dto.title,
+                createdById: userId
+            }
+        })
     }
 }
