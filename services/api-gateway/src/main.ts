18c4304910c21f2003c9ff25cf9f2afe4896a628
finalSay9 <chimwazaevan1@gmail.com>
2026-09-17 16:00:48 +0200

    created the real collab editor it working

--- services/api-gateway/src/main.ts
+++ services/api-gateway/src/main.ts
@@ -2,14 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
-  
-  
   const app = await NestFactory.create(AppModule);
-   app.enableCors({
-    origin: 'http://localhost:5173',
+  
+  // Allow both origins here 👇
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://192.168.43.219:5173'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
